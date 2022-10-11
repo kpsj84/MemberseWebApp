f56433f23e96d5c047a347caaa0b5ee9b9b64321
Kamal Jhinjer <kpsj84>
2022-10-11 11:08:54 +0530

    Social Media 4 new Test Cases

--- src/main/java/WebPageObjects/ExplorePage.java
+++ src/main/java/WebPageObjects/ExplorePage.java
@@ -28,5 +28,54 @@ public class ExplorePage {
 	public WebElement SignupButton() {
 		return SignupButton;
 	}
+	
+	 @FindBy(xpath="//*[@id=\"__next\"]/div[3]/div/div[1]/div[2]/div/div/div[3]/div/a[1]")
+	    private WebElement FacebookIcon;
+
+	    public WebElement FacebookIcon() {
+	    	return FacebookIcon;
+	    }
+
+	    @FindBy(xpath="/html/body/div[1]/div[3]/div/div[1]/div[2]/div/div/div[3]/div/a[2]")
+	    private WebElement InstagramIcon;
+
+	    public WebElement InstagramIcon() {
+	    	return InstagramIcon;
+	    }
+	    
+	    @FindBy(xpath="//*[@id=\"__next\"]/div[3]/div/div[1]/div[2]/div/div/div[3]/div/a[3]")
+	    private WebElement LinkedinIcon;
+
+	    public WebElement LinkedinIcon() {
+	    	return LinkedinIcon;
+	    }
+	    
+	    @FindBy(xpath="//*[@id=\"__next\"]/div[3]/div/div[1]/div[2]/div/div/div[3]/div/a[4]")
+	    private WebElement TikTokIcon;
+
+	    public WebElement TikTokIcon() {
+	    	return TikTokIcon;
+	    }
+	          
+	    @FindBy(xpath="//*[@id=\"__next\"]/div[3]/div/div[1]/div[2]/div/div/div[4]/div/a[1]")
+	    private WebElement TermLink;
+
+	    public WebElement TermLink() {
+	    	return TermLink;   
+	    }     
+	        
+	    @FindBy(xpath="//*[@id=\"__next\"]/div[3]/div/div[1]/div[2]/div/div/div[4]/div/a[2]")
+	    private WebElement GuidelinesLink;
+
+	    public WebElement GuidelinesLink() {
+	        return GuidelinesLink;
+	    }  
+	        
+	    @FindBy(xpath="//*[@id=\"__next\"]/div[3]/div/div[1]/div[2]/div/div/div[4]/div/a[3]")
+	    private WebElement PrivacyLink;
+
+	    public WebElement PrivacyLink() {
+	        return PrivacyLink;
+	    }
 
 }
---
+++ src/test/java/Memberse/WebAppAutomation/wLaunchMemberseFacebookTest.java
@@ -0,0 +1,40 @@
+package Memberse.WebAppAutomation;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import WebPageObjects.ExplorePage;
+
+public class wLaunchMemberseFacebookTest extends WBase {
+	
+	@Test
+	public void wLaunchMemberseFacebookTestCase()throws InterruptedException {
+		Thread.sleep(10000);
+		
+		ExplorePage fb = new ExplorePage(driver);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		fb.FacebookIcon().click();
+		Thread.sleep(3000);
+		
+		 String mainWindowHandle = driver.getWindowHandle();
+	     Set<String> allWindowHandles = driver.getWindowHandles();
+	     Iterator<String> iterator = allWindowHandles.iterator();
+	     while (iterator.hasNext()) {
+	    	 String ChildWindow = iterator.next();
+	         	if (!mainWindowHandle.equalsIgnoreCase(ChildWindow)) {
+	            driver.switchTo().window(ChildWindow);
+		
+	    driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+ 	    String Text= driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div/div[2]/div/div/div[1]/span/h1")).getText();
+ 		System.out.println(Text);
+ 		Assert.assertEquals(Text,"Memberse");
+	         	}
+	    }
+	}
+
+}
