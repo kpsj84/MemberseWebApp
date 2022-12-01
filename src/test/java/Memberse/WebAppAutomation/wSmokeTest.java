9034e7633aa32826e2f400454f2e0c43e78a4076
Kamal <kpsj84@gmail.com>
2022-12-01 14:23:11 +0530

    start refactor 1

--- src/test/java/Memberse/WebAppAutomation/wSmokeTest.java
+++ src/test/java/Memberse/WebAppAutomation/wSmokeTest.java
@@ -13,43 +13,58 @@ public class wSmokeTest extends WBase{
 	@Test
 	public void wSmokeTestCase() throws InterruptedException {
 		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		Thread.sleep(15000);
+		Thread.sleep(7000);
+		
+		String verifyExploreText = driver.findElement(By.xpath("//*[text()='Find your Content Creator']")).getText();
+		System.out.println(verifyExploreText);
+		Assert.assertEquals(verifyExploreText, "Find your Content Creator");
 		
 		WebElement searchField = driver.findElement(By.xpath("//*[@id='name']"));
 		JavascriptExecutor js = (JavascriptExecutor)driver;
 		js.executeScript("arguments[0].click()", searchField);
 		String testText = "kqatest";
 		searchField.sendKeys(testText);
+		Thread.sleep(5000);
 		
-		//driver.findElement(By.xpath("//span[text()='Trending']")).click();
-		Thread.sleep(15000);
-		int Value = driver.findElements(By.className("space-y-2")).size();
-		if(Value == 0)
+		String noResult = null;
+		try
 		{
-			System.out.println("Size of Result is Nill");
-			Assert.assertFalse(true);
+			noResult = driver.findElement(By.xpath("//*[text()='No results available.']")).getText();
+			System.out.println(noResult);
+			Assert.assertEquals(noResult, "No results available.");
 		}
-		else
+		catch(Exception e)
 		{
-			System.out.println(Value);
+			
+		}
+		finally
+		{
+			if(noResult == "No results available.")
+			{
+				System.out.println(noResult);
+			}
+			else
+			{
+					int Value = driver.findElements(By.xpath("(//*[@class='space-y-2'])[1]/div")).size();
+					System.out.println("Results fetched from search = " + Value);
+			}
 		}
 		
 		WelcomePage wp = new WelcomePage(driver);
 		wp.LoginButton().click();
-		Thread.sleep(3000);
+		Thread.sleep(1000);
 		driver.findElement(By.xpath("//input[@name='email']")).sendKeys(testText);
 		driver.navigate().back();
 		
 		wp.SignupButtton().click();
-		Thread.sleep(3000);
+		Thread.sleep(1000);
 		driver.findElement(By.xpath("//input[@name='email']")).sendKeys(testText);
 		driver.navigate().back();
-		Thread.sleep(3000);
+		Thread.sleep(1000);
 		
 		String apps = driver.findElement(By.xpath("//p[text()='Download the Memberse app for an optimized experience']")).getText();
 		Assert.assertEquals(apps, "Download the Memberse app for an optimized experience");
 		System.out.println("Smoke Test Pass for Web App");
-		System.out.println("Web hook trigger one");
 	}
 
 }
