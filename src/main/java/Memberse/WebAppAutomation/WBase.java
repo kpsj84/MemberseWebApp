1f4b250705d43414b9ef223078c356f08b962302
Kamal <kpsj84@gmail.com>
2022-12-28 13:44:34 +0530

    27 TC finetune intel jenkins true

--- src/main/java/Memberse/WebAppAutomation/WBase.java
+++ src/main/java/Memberse/WebAppAutomation/WBase.java
@@ -15,7 +15,7 @@ import org.testng.annotations.BeforeTest;
 
 public class WBase {
 	
-	public static boolean intelJenkinsServer = false;
+	public static boolean intelJenkinsServer = true;
 	
 	//Initiate Variables
 	public static ChromeOptions   options;
