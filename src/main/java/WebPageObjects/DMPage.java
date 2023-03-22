6c0ccc4fc812f27f07e669c8ac0b372d812fc166
Kamal <kpsj84@gmail.com>
2023-03-22 12:07:31 +0530

    Automation of DM functionality

---
+++ src/main/java/WebPageObjects/DMPage.java
@@ -0,0 +1,46 @@
+package WebPageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DMPage {
+	
+	WebDriver localDriver;
+	
+	public DMPage(WebDriver driver)
+	{
+		this.localDriver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(css="button[class*='group relative w-full flex'] span[class='text-base font-medium leading-4']")   
+	private WebElement writeMessage;
+	
+	public WebElement writeMessage() {
+		return writeMessage;
+	}
+	
+	@FindBy(xpath="//textarea[@name='content']")   
+	private WebElement dmText;
+	
+	public WebElement dmText() {
+		return dmText;
+	}
+	
+	@FindBy(xpath="//span[contains(text(), 'Preview')]")   
+	private WebElement previewButton;
+	
+	public WebElement previewButton() {
+		return previewButton;
+	}
+	
+	@FindBy(xpath="//*[@data-icon='paper-plane']")   
+	private WebElement sendDM;
+	
+	public WebElement sendDM() {
+		return sendDM;
+	}
+
+}
